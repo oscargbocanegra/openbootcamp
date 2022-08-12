7066500734617eb44dd491a5af8e169c11807a99
oscar giovanni <oscargiovanni@gmail.com>
2022-08-11 21:54:12 -0500

    add folder taller

---
+++ bootcamplearnjava/src/com/company/TiposDatos.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class TiposDatos {
+    // 1. Numericos
+
+    // 1.1 Enteros
+    byte variable1 = 5;
+    short variable2 = 10;
+    int variable3 = 30;
+    long variable4 = 100;
+
+    // 1.2 Decimales
+    float variable5 = 5.5f;
+    double variable6 = 10.5d;
+
+    //2. Booleano
+    boolean variable7 = false;
+    boolean Variable8 = true;
+
+    // 3. Texto
+    char variable9 = 'a';
+    String variable10 = "Lorem ipsum dolor siet amet. ........";
+
+}
---
+++ talleresbootcamp/tema1/DatosMain.java
@@ -0,0 +1,43 @@
+package Ejercicios.tema1;
+
+public class DatosMain {
+
+    public static void main(String[] args) {
+
+       // 1. Datos de tipo Numerico
+       // 1.1 Enteros
+        byte variable1 = 5;
+        System.out.println("Este es un dato de tipo byte con valor asignado : " + variable1);
+
+        short variable2 = 10;
+        System.out.println("Este es un dato de tipo short con valor asignado : " + variable2);
+
+        int variable3 = 30;
+        System.out.println("Este es un dato de tipo int con valor asignado : " + variable3);
+
+        long variable4 = 100;
+        System.out.println("Este es un dato de tipo long con valor asignado : " + variable4);
+
+        // 1.2 Decimales
+        float variable5 = 5.5f;
+        System.out.println("Este es un dato de float valor asignado : " + variable5);
+
+        double variable6 = 10.5d;
+        System.out.println("Este es un dato de tipo double con valor asignado : " + variable6);
+
+        //2. Booleano
+        boolean variable7 = false;
+        System.out.println("Este es un dato de tipo boolean con valor asignado : " + variable7);
+
+        boolean variable8 = true;
+        System.out.println("Este es un dato de tipo boolean con valor asignado : " + variable8);
+
+        // 3. Texto
+        char variable9 = 'a';
+        System.out.println("Este es un dato de tipo char con valor asignado : " + variable9);
+
+        String variable10 = "Lorem ipsum dolor siet amet. ........";
+        System.out.println("Este es un dato de tipo String con valor asignado : " + variable10);
+
+        }
+}
